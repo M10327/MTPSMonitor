3c797014e0ce231082c7f8af4f33e1cce5742979
M10327 <Melody@silverportreborn.com>
2022-09-16 23:35:58 -0500

    Add project files.

---
+++ MTPSMonitor/MTPSMonitor.cs
@@ -0,0 +1,38 @@
+﻿using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MTPSMonitor
+{
+    public class MTPSMonitor : RocketPlugin<config>
+    {
+        private static Timer aTimer;
+        protected override void Load()
+        {
+            aTimer = new System.Timers.Timer(Configuration.Instance.DelayBetweenChecks);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (!Level.isLoaded) return;
+            if (Provider.debugTPS <= Configuration.Instance.WarnThreshhold)
+            {
+                Rocket.Core.Logging.Logger.LogWarning($"TPS Drop detected. Current TPS: {Provider.debugTPS}");
+            }
+        }
+
+        protected override void Unload()
+        {
+            aTimer.Stop();
+            aTimer.Elapsed -= OnTimedEvent;
+        }
+    }
+}
---
+++ MTPSMonitor/config.cs
@@ -0,0 +1,20 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTPSMonitor
+{
+    public class config : IRocketPluginConfiguration
+    {
+        public int DelayBetweenChecks;
+        public int WarnThreshhold;
+        public void LoadDefaults()
+        {
+            DelayBetweenChecks = 500;
+            WarnThreshhold = 45;
+        }
+    }
+}
